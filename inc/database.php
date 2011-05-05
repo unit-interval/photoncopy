5ac05ebcee465c77a29f5c335907030fff5213b7
Tao <tech@huangtao.me>
2011-05-05 13:06:15 +0800

    move mysqli initialization to inc/database.php

---
+++ inc/database.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once '../config.php';
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($db->connect_error)
+err_redir("mysql connect error({$db->connect_errno}).",'/error.php');
+if (!$db->set_charset("utf8"))
+err_redir("db error({$db->errno}).", '/error.php');
+
