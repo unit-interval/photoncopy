5709a2f47f1722b68e8e27f267d1ec16bbfb38da
roy <libragold@gmail.com>
2011-05-05 17:06:03 +0800

    add local configuration and static

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+/** URL BASE and etc.*/
+define('SERVER_HOST', 'pq.planetb612.info');
+define('URL_BASE', 'http://' . SERVER_HOST . '/');
+define('CODE_NAME', '光子复制');
+
+/** session name */
+define('SESSNAME', 'photonsess');
+
+/** DB */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'pq');
+define('DB_PASS', 'pq');
+define('DB_NAME', 'pq');
+
+/** salt */
+define('SALT_REG', 'WingsOfLiberty');
+
+/** swith for debug mode */
+//$debug_enabled = FALSE;
+
+/** array that contains page meta info */
+$link = array(
+	'css' => array(),
+	'js' => array(),
+	'script' => array(),
+);
+
+?>
