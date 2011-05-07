2a65b67ff1f5b0569419b45eb3e004fd18b54293
Tao <tech@huangtao.me>
2011-05-07 23:06:02 +0800

    update submit.php, temp

--- process.php
+++ process.php
@@ -8,31 +8,29 @@ session_name(SESSNAME_P);
 session_start();
 
 if($_SESSION['partner'] !== true)
-die;
+err_redir('未登錄', '/partner.php');
 
 if(isset($_GET['oid']) && isset($_GET['act'])) {
 	$oid = intval($_GET['oid']);
 	$act = intval($_GET['act']);
 } else
-die;
+err_redir('無效訂單', '/partner.php');
 
 $query = "select `status` from `order`
 	where `id` = $oid and `pid` = {$_SESSION['pid']}";
 $result = $db->query($query);
 if($result->num_rows == 0)
-die;
+err_redir('無效訂單', '/partner.php');
 
 $row = $result->fetch_row();
 $result->free();
 
-$to = text_queue_action_par($row[0]);
-
 if(!in_array($act, text_queue_action_par($row[0])))
-die;
+err_redir('無效訂單', '/partner.php');
 
 $query = "update `order` set `status` = ".to_status_par($act)."
 	where `id` = $oid";
 if($db->query($query) !== TRUE)
 	err_redir("db error({$db->errno}).", '/error.php');
 
-err_redir('', '/partner.php');
+err_redir('成功修改訂單狀態', '/partner.php');
