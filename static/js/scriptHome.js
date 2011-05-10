b03ed62f41be5540c270dcff5206b63287a81d0f
Tao <tech@huangtao.me>
2011-05-11 02:25:01 +0800

    slider bug fix, partial

--- static/js/scriptHome.js
+++ static/js/scriptHome.js
@@ -1,3 +1,8 @@
+
+var page=[['页数很少','1-10页',1],['页数较少','10-50页',10],['页数多','50-200页',50],['页数较多', '200-500页',200],['页数很多','500-1000页',500], ['页数非常多','1000页以上'],1000];
+var copy=[1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000];
+var $slider1;
+
 $(function(){
 	
 	$('#btn1').hover(function(){
@@ -26,26 +31,28 @@ $(function(){
 		$(this).addClass('selected');
 	});
 	
-	var page=[['页数很少','1-10页',1],['页数较少','10-50页',10],['页数多','50-200页',50],['页数较多', '200-500页',200],['页数很多','500-1000页',500], ['页数非常多','1000页以上'],1000];
-	var copy=[1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000];
-	
 	function refreshCredit(){
 		
 	}
 	
-	$("div#dynos-slider").slider({
+	$slider1 = $("div#dynos-slider").slider({
 		orientation: "vertical",
 		range: "min",
 		min: 0,
 		max: 5,
 		value: 0,
 		slide: function(event, ui){
-			var tmpValue=page[ui.value];
-			$('span#dynos-handle-inner-dynos').html(tmpValue[0]);
-			$("span#dynos-handle-inner-price").html(tmpValue[1]);
-			refreshCredit();
+			setSlider1(ui.value);
 		}
 	});
+
+	$slider1.find('a').html(
+			'<span id="dynos-handle-inner">'+
+			'<span id="dynos-handle-inner-dynos"></span>'+
+			'<span id="dynos-handle-inner-price"></span>'+
+			'</span>'
+			)
+		.removeAttr('href');
 	
 	$("#workers-slider").slider({
 		orientation: "vertical",
@@ -59,4 +66,14 @@ $(function(){
 		}
 	});
 
+	setSlider1(1);
+
 })
+
+function setSlider1(qty) {
+	var tmpValue = page[qty];
+	$slider1.slider('value', qty);
+	$('span#dynos-handle-inner-dynos').html(tmpValue[0]);
+	$("span#dynos-handle-inner-price").html(tmpValue[1]);
+//	refreshCredit();
+}
