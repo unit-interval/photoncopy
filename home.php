bac87f42b1a6995c0be7e05b05eada32aff7969d
Tao <tech@huangtao.me>
2011-05-23 21:27:21 +0800

    home: cancel order finished

--- home.php
+++ home.php
@@ -24,6 +24,7 @@ $link['css'][] = 'styleHome';
 $link['js'][] = 'jquery';
 $link['js'][] = 'jquery-ui';
 $link['js'][] = 'jquery.scrollTo';
+$link['js'][] = 'script';
 $link['js'][] = 'scriptHome';
 
 $orders = array();
