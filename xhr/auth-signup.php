a466aef9adf847b57d6b026e409d67fa8d31aa79
roy <libragold@gmail.com>
2011-05-28 11:38:37 +0800

    auth email done

--- xhr/auth-signup.php
+++ xhr/auth-signup.php
@@ -18,23 +18,38 @@ function send_reg_mail($to) {
 	$addr = base64_encode($to);
 	$hash = md5( SALT_REG . $addr );
 	$link = URL_BASE . "authorize.php?c=signup&a=" . rawurlencode($addr). "&v=$hash";
-	$subject = CODE_NAME . ' - 註冊';
+	$subject = CODE_NAME . ' - 注册确认函';
 	$body = <<<EOT
-To continue your registration, click the link below, plz.
-	
-	$link
-
-photoncopy is blahblah.
-and this mail will be beautified with html.
-
-Cheers,
-
-photoncopy team
-
-=-=-=
-中文字符測試
-=-=-=
-
+		<table width="650" align="center" style="color: #444; margin: 30px auto; font-size: 14px; font-family: 'Microsoft Yahei',Tahoma,Arial,Helvetica,STHeiti; box-shadow: 0 0 10px gray; border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px; overflow: hidden" cellpadding="0" cellspacing="0">
+			<tbody><tr id="header">
+				<td height="102" background="/media/images/email/header_bg.png" bgcolor="#E6F1FB" align="center">
+					<table width="95%">
+						<tbody><tr>
+							<td align="left">
+								<img src="http://photoncopy.com/media/images/email/email_logo.png">
+							</td>
+						</tr>
+					</tbody></table>
+				</td>
+			</tr>
+			<tr id="content">
+				<td bgcolor="#F4FAFF" align="center">
+					<table width="95%" cellpadding="30">
+						<tbody><tr>
+							<td align="left">
+								您好，<br>
+								<br>
+								感谢您注册光子复制，请点击确认<a style="color: #1F75CC; text-decoration: none" href="$link" target="_blank">绑定邮箱</a>。
+								<br><br><font size="-1" color="gray">如果链接无效，请将以下链接复制到浏览器地址栏访问：<br>$link</font>
+								<br>
+								<br>
+								光子复制团队 敬上<br>
+							</td>
+						</tr>
+					</tbody></table>
+				</td>
+			</tr>
+		</tbody></table>	
 EOT;
 	$header = "Content-type: text/plain; charset=utf-8\r\n";
 	$header .= "From: " . CODE_NAME . " <noreply-reg@" .
