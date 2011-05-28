b9d3e56a34979318df95af7c02b29e01959e8014
roy <libragold@gmail.com>
2011-05-29 00:59:40 +0800

    confirm email added

--- authorize.php
+++ authorize.php
@@ -4,6 +4,81 @@ include './config.php';
 include DIR_INC . 'database.php';
 include DIR_INC . 'function.php';
 
+function send_confirm_email($to, $name){
+	$addr = base64_encode($to);
+	$subject = CODE_NAME . ' - 欢迎来到光子复制';
+	$body = <<<EOT
+		<table width="650" align="center" style="color: #444; margin: 30px auto; font-size: 14px; font-family: 'Microsoft Yahei',Tahoma,Arial,Helvetica,STHeiti; box-shadow: 0 0 10px gray; border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px; overflow: hidden" cellpadding="0" cellspacing="0">
+			<tr id="header">
+				<td height="102" background="http://photoncopy.com/media/images/email/header_bg.png" bgcolor="#E6F1FB" align="center">
+					<table width="95%">
+						<tr>
+							<td align="left">
+								<img src="http://photoncopy.com/media/images/email/email_logo.png" />
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr id="content">
+				<td bgcolor="#F4FAFF" align="center">
+					<table width="95%" cellpadding="30">
+						<tr>
+							<td align="left">
+								$name ，您好，<br />
+								<br />
+								欢迎来到光子复制，光子复制为您提供如下服务：<br />
+								<br />
+								<table width='100%'>
+									<tr>
+										<td width="20"></td>
+										<td>
+											<font size='+1' color='#1F75CC'>让文印服务更便捷</font>
+											<br />
+											<img style='float: left; margin: 10px' width='64px' height='64px' src='http://photoncopy.com/media/images/email/paper_plane.png' alt='paper plane'>
+											<br />还记得在打印店焦急等待的时光吗？还记得无数遗忘在打印店的U盘吗？还记得怀揣一把钢镚时的尴尬吗？时代在变，现在只需在线提交打印订单，行走间一切已经完成。<a style='color: #1F75CC; text-decoration: none' href='http://photoncopy.com/blog/tutorial' target='_blank'>想了解更多？</a>
+										</td>
+									</tr>
+									<tr>
+										<td width="20"></td>
+										<td>
+											<font size='+1' color='#1F75CC'>让用户体验更完美</font>
+											<br />
+											<img style='float: left; margin: 10px' width='64px' width='64px' height='64px' src='http://photoncopy.com/media/images/email/chrome.png' alt='google chrome'>
+											<br />光子复制采用现代的互联网语言编写，为了让您获得更好的用户体验。
+											<br />在此我们推荐您使用Google Chrome浏览器（<a style='color: #1F75CC; text-decoration: none' href='http://www.google.com/chrome' target='_blank'>了解 Google Chrome</a>）。
+											<br />我们推荐IE控用户使用360极速浏览器（<a style='color: #1F75CC; text-decoration: none' href='http://chrome.360.cn/' target='_blank'>了解 360极速</a>）。
+										</td>
+									</tr>
+									<tr>
+										<td width="20"></td>
+										<td>
+											<font size='+1' color='#1F75CC'>一切都是免费的</font>
+											<br />
+											<img style='float: left; margin: 10px' width='64px' width='64px' height='64px' src='http://photoncopy.com/media/images/email/free.png' alt='free'>
+											<br />由于网站创始人都是大学生，所以光子复制可能是最了解您的需求。
+											<br />并且本着服务大学生的想法，您得到的一切服务都是免费的。
+											<br />在此我们期望您能为我们的网站添砖加瓦，贡献您的一份力量。
+										</td>
+									</tr>
+								</table><br />想了解更多光子复制的相关讯息？请来<a style='color: #1F75CC; text-decoration: none' href='http://photoncopy.com/blog' target='_blank'>黑板报</a>看看。<br />
+								<br />
+								光子复制团队 敬上<br />
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+EOT;
+	$header = "Content-type: text/html; charset=utf-8\r\n";
+	$header .= "From: " . CODE_NAME . " <service@" .
+	SERVER_HOST . ">\r\n";
+	$header .= "Reply-To: " . CODE_NAME . " <service@" .
+	SERVER_HOST . ">\r\n";
+	return mail($to, $subject, $body, $header);
+}
+
 function verify_link_reset() {
 	return (($_GET['v'] == md5(SALT_REG . $_GET['a'] . $_GET['t'])) && (date_diff(date_create('@' . $_GET['t']), date_create())->h < 4));
 }
@@ -35,7 +110,7 @@ session_start();
 
 if($_GET['c'] == 'login') {
 	if(!($input = verify_login_form()))
-		err_redir('Invalid Login Information.');
+		err_redir('邮箱或密码输入有误，请重新登录');
 	$query = "select `id`, `passwd`, `name` from `user`
 		where `email` = '{$db->real_escape_string($input['email'])}'";
 	if(($result = $db->query($query)) && ($result->num_rows > 0)) {
@@ -45,7 +120,7 @@ if($_GET['c'] == 'login') {
 	} else
 		$logged_in = false;
 	if(!$logged_in)
-		err_redir('login fail.');
+		err_redir('登录失败，用户名或密码有误，请重新登录');
 	$query = "select `pid`, `credit` from `credit` where `uid` = {$user['id']}";
 	if(!($result = $db->query($query)))
 		err_redir("db error({$db->errno}). query:$query", '/error.php');
@@ -77,16 +152,16 @@ if($_GET['c'] == 'login') {
 	err_redir();
 } elseif($_GET['c'] == 'signup' && isset($_GET['a']) && isset($_GET['v'])) {
 	if(!verify_link_signup())
-		err_redir('invalid activation link.');
+		err_redir('您访问的激活链接无效');
 	$email = base64_decode($_GET['a']);
 	if(user_exists($email))
-		err_redir("email already activated. please login. ($email)");
+		err_redir("邮箱($email)已在光子复制注册，请直接登录");
 	$_SESSION['email'] = $email;
 	$_SESSION['state'] = 'activate';
 	err_redir('Welcome!', '/profile.php');
 } elseif($_GET['c'] == 'reset' && isset($_GET['a']) && isset($_GET['t']) && isset($_GET['v'])) {
 	if(!verify_link_reset())
-		err_redir('your password reset link has expired.');
+		err_redir('您的密码重置链接已失效');
 	$email = base64_decode($_GET['a']);
 	if(!user_exists($email))
 		err_redir("user not found. ($email)");
@@ -105,7 +180,7 @@ if($_GET['c'] == 'login') {
 	} else
 		$logged_in = false;
 	if(!$logged_in)
-		err_redir('login fail.','/partner.php');
+		err_redir('抱歉，登录失败','/partner.php');
 	$query = "select `uid`, `credit` from `credit` where `pid` = {$user['id']}";
 	if(!($result = $db->query($query)))
 		err_redir("db error({$db->errno}). query:$query", '/error.php');
@@ -134,7 +209,7 @@ if($_GET['c'] == 'login') {
 	err_redir();
 } elseif($_SESSION['state'] === 'activate') {
 	if(!($input = verify_signup_form()))
-		err_redir('invalid input', '/profile.php');
+		err_redir('您提供的信息有误，请重新输入', '/profile.php');
 	$query = "insert into `user` values (
 		default,
 		'{$db->real_escape_string($input['email'])}',
@@ -145,15 +220,16 @@ if($_GET['c'] == 'login') {
 	if($db->query($query) !== TRUE)
 		err_redir("db error({$db->errno}).", '/error.php');
 	$uid = $db->insert_id;
-	$query = "insert into `credit` values ($uid, 0, 50)";
+	$query = "insert into `credit` values ($uid, 0, 10)";
 	if($db->query($query) !== TRUE)
 		err_redir("db error({$db->errno}).", '/error.php');
 	unset($_SESSION['state']);
 	$_SESSION['logged_in'] = true;
 	$_SESSION['uid'] = $uid;
 	$_SESSION['name'] = $input['name'];
-	$_SESSION['credit'] = array(0 => 50);
-	err_redir('Conguratuations, your account is activated.', '/home.php');
+	$_SESSION['credit'] = array(0 => 10);
+	send_confirm_email($_SESSION['email'], $_SESSION['name']);
+	err_redir('恭喜您已成功注册光子复制帐号', '/home.php');
 } elseif($_SESSION['state'] === 'resetpw') {
 } else {
 	err_redir();
