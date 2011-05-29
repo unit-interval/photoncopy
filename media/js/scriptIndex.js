de863062e8b02600f2c4edaee58e1139a46677fd
roy <libragold@gmail.com>
2011-05-29 17:32:07 +0800

    add chinese guidance

--- media/js/scriptIndex.js
+++ media/js/scriptIndex.js
@@ -28,13 +28,13 @@ $(document).ready(function(){
 			statusCode: {
 				200: function(data) {
 						if(data.errno == 0)
-							Notification.add('email successfully sent.');
+							Notification.add('邮件已成功发送，请查收注册确认函');
 						else if(data.errno == 3)
-							Notification.add('user already exists.');
+							Notification.add('邮箱已被注册，请直接登录');
 						else if(data.errno == 1)
-							Notification.add('invalid input.');
+							Notification.add('邮箱输入有误，请重新输入');
 						else
-							Notification.add('Ooops.');
+							Notification.add('啊哦，服务器开小差了，请稍候访问');
 					 },
 			},
 		});
@@ -53,13 +53,13 @@ $(document).ready(function(){
 			statusCode: {
 				200: function(data) {
 						if(data.errno == 0)
-							Notification.add('email successfully sent.');
+							Notification.add('邮件已成功发送，请查收重设密码邮件');
 						else if(data.errno == 3)
-							Notification.add('user doesn\'t exist.');
+							Notification.add('需要找回密码的邮箱尚未被注册');
 						else if(data.errno == 1)
-							Notification.add('invalid input.');
+							Notification.add('邮箱输入有误，请重新输入');
 						else
-							Notification.add('Ooops.');
+							Notification.add('啊哦，服务器开小差了，请稍候访问');
 					 },
 			},
 		});
