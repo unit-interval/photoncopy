a568ef2de357b28e59980777ffc4a7cffe58e4e4
Tao <tech@huangtao.me>
2011-06-02 14:36:21 +0800

    file.php bugfix

--- admin/mirror/file.php
+++ admin/mirror/file.php
@@ -9,7 +9,7 @@ if(!isset($_GET['q'])) {
 	die;
 }
 $fn = basename($_GET['q']);
-$url = QUERY_BASE . $fn;
+$url = urlencode(QUERY_BASE . $fn);
 if(!($handle = @fopen($url, 'r'))) {
 	header('HTTP/1.0 400 Bad Request');
 	die('文件不存在: ' . $url);
