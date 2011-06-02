36b99ccae1f57be6158a14e1c83eaa05427cd8a2
Tao <tech@huangtao.me>
2011-06-02 14:40:42 +0800

    file.php bugfix

--- admin/mirror/file.php
+++ admin/mirror/file.php
@@ -9,13 +9,14 @@ if(!isset($_GET['q'])) {
 	die;
 }
 $fn = basename($_GET['q']);
-$url = QUERY_BASE . urlencode($fn);
+//$url = QUERY_BASE . urlencode($fn);
+$url = QUERY_BASE . $fn;
 if(!($handle = @fopen($url, 'r'))) {
 	header('HTTP/1.0 400 Bad Request');
 	die('文件不存在: ' . $url);
 }
 header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="' . $fn . '"');
+header('Content-Disposition: attachment; filename="' . urldecode($fn) . '"');
 while(!feof($handle)) {
 	echo fread($handle, 8192);
 	flush();
