01ded9ee36a8ec44afdf01cc3f5cd9ae97016288
ZHANG Ta <coolgene@gmail.com>
2011-07-27 02:42:47 +0800

    minor fix #32 added renren, weibo accont connection, but not fully available now.

--- xhr/auth-login-renren.php
+++ xhr/auth-login-renren.php
@@ -6,11 +6,11 @@
 ini_set( "DISPLAY_ERRORS ",   "1 "); 
 error_reporting(E_ALL);  
 
-include '../config.php';
-include DIR_INC . 'database.php';
-include DIR_INC . 'function.php';
+#include '../config.php';
+#include DIR_INC . 'database.php';
+#include DIR_INC . 'function.php';
 require_once DIR_INC .'oauth/connect-renren.php';
-session_name(SESSNAME);
+#session_name(SESSNAME);
 session_start();
 
 $oauth = new RenRenOauth();
