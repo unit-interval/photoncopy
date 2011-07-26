0ce9af67dd628e7e65b69a65f9449bb3247fb3bc
ZHANG Ta <coolgene@gmail.com>
2011-07-27 03:05:06 +0800

    minor fix #32 added renren, weibo accont connection, but not fully available now.

--- login-sns.php
+++ login-sns.php
@@ -1,4 +1,5 @@
 <ul>
+	<li>需要 cURL！</li>
 	<li><a href="https://graph.renren.com/oauth/authorize?response_type=code&client_id=3626a6bb44aa4c94833c6f5f7113608b&redirect_uri=http%3A%2F%2Ftesting.photoncopy.com%2Fxhr%2Fauth-login-renren.php">用人人网帐号登陆</a></li>
 	<li><a href="xhr/auth-login-sina.php?c=login">用新浪微博帐号登陆</a></li>
 		<li><a href="">用腾讯 QQ 帐号登陆（未完成）</a></li>
