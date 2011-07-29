3399b8bb6fe9f4fa555c0112feebeab5a7a9000d
Zilin Jiang <libragold@gmail.com>
2011-07-29 11:43:26 +0800

    new signup.html, check it out

---
+++ media/js/huozr.js
@@ -0,0 +1,10 @@
+$(function(){
+    $.scrollTo($('#login #content'), {
+        duration: 0,
+        offset: { top: -70 }
+    });
+    $.scrollTo($('#signup #content'), {
+        duration: 0,
+        offset: { top: -70 }
+    });
+})
